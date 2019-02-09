b3ae411727b75d3d3a698725170136551b04f313
Andrew Gionfriddo <andrew@betterpt.com>
2019-02-09 16:47:41 -0500

    simple global store

---
+++ simple-global-store/src/components/NumberSubmit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="number" v-model="numberInput">
+    <button 
+      @click="addNumber(numberInput)">
+      Add new number
+    </button>
+  </div>
+</template>
+
+<script>
+import { store } from '../store.js';
+export default {
+  name: 'NumberSubmit',
+  data() {
+    return {
+      number: 0
+    }
+  },
+  methods: {
+    addNumber(numberInput) {
+      store.addNumber(Number(numberInput))
+    }
+  }
+}
+</script>
+
