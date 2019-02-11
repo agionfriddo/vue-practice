d20a8252593c2990dfae7fe8b52464666931ec67
Andrew Gionfriddo <andrew@betterpt.com>
2019-02-11 09:06:44 -0500

    Add routing and vuex examples

---
+++ vuex-example/src/components/ParentComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NumberDisplay />
+    <NumberSubmit />
+  </div>
+</template>
+
+<script>
+import NumberDisplay from "./NumberDisplay";
+import NumberSubmit from './NumberSubmit';
+export default {
+  name: 'ParentComponent',
+  data() {
+    return {
+      numbers: [1, 2, 3]
+    }
+  },
+  components: {
+    NumberDisplay,
+    NumberSubmit
+  }
+}
+</script>
